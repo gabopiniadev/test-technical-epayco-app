74f02b0efebaa7a1ff7c14ddcda52a7315cbb091
Gabriel Alejandro Pina <gabrielpinia@hotmail.com>
2025-02-05 00:49:28 -0400

    remove alert innecesaries

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import React from "react";
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "../src/feature/auth/pages/login/Login";
 import PrivateRoute from "./feature/auth/security/private-route/PrivateRoute.tsx";
